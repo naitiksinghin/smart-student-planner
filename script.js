e28c9bc76f7fd8d75909ec0c0e9f757660a92a9c
Naitik.Singh <naitik.singhin@gmail.com>
2026-01-31 18:01:06 +0530

    Initial commit - Smart Student Planner

---
+++ script.js
@@ -0,0 +1,87 @@
+let subjects = JSON.parse(localStorage.getItem("subjects")) || [];
+
+function addSubject() {
+    let subject = document.getElementById("subject").value;
+    let total = Number(document.getElementById("total").value);
+    let attended = Number(document.getElementById("attended").value);
+
+    if (subject === "" || total === 0) {
+        alert("Please enter valid details");
+        return;
+    }
+
+    let percentage = ((attended / total) * 100).toFixed(2);
+    let message = "";
+
+if (percentage >= 75) {
+    let bunkAllowed = Math.floor((attended / 0.75) - total);
+    message = `✅ Safe | Can bunk ${bunkAllowed} classes`;
+} else {
+    let needed = Math.ceil((0.75 * total - attended) / (1 - 0.75));
+    message = `⚠️ Need ${needed} more classes`;
+}
+
+
+    subjects.push({ subject, percentage, message });
+
+
+    localStorage.setItem("subjects", JSON.stringify(subjects));
+    displaySubjects();
+    drawChart();
+    document.getElementById("subject").value = "";
+    document.getElementById("total").value = "";
+    document.getElementById("attended").value = "";
+ 
+
+}
+
+function displaySubjects() {
+    let tableBody = document.getElementById("tableBody");
+    tableBody.innerHTML = "";
+
+    subjects.forEach((item, index) => {
+        let row = `
+            <tr>
+                <td>${item.subject}</td>
+                <td>${item.percentage}%</td>
+                <td>${item.message}</td>
+                <td><button onclick="deleteSubject(${index})">❌ Delete</button></td>
+            </tr>
+        `;
+        tableBody.innerHTML += row;
+    });
+}
+displaySubjects();
+drawChart();
+
+function deleteSubject(index) {
+    subjects.splice(index, 1);
+    localStorage.setItem("subjects", JSON.stringify(subjects));
+    displaySubjects();
+    drawChart();
+}
+function drawChart() {
+    let ctx = document.getElementById("attendanceChart").getContext("2d");
+
+    let labels = subjects.map(s => s.subject);
+    let data = subjects.map(s => s.percentage);
+
+    if (window.myChart) {
+        window.myChart.destroy();
+    }
+
+    window.myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [{
+                label: "Attendance %",
+                data: data
+            }]
+        }
+    });
+}
+
+
+
+
